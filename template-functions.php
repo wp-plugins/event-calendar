439e6a187106f7fc6cdfcd7e5e92b0c3eb266d24
alex_tingle <alex_tingle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-24 10:53:38 +0000

    Don't show pages in the calendar. Thanks to Olaf Klejnstrup-Jensen for reporting the bug.
    git-svn-id: https://plugins.svn.wordpress.org/event-calendar/trunk@70269 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- template-functions.php
+++ template-functions.php
@@ -121,6 +121,7 @@ function ec3_util_calendar_days($begin_month_id,$end_month_id)
        1 AS is_event
      FROM $wpdb->posts,$ec3->schedule
      WHERE post_status='publish'
+       AND post_type='post'
        AND post_id=id
        AND end>='$begin_date'
        AND start<'$end_date'";
@@ -137,6 +138,7 @@ function ec3_util_calendar_days($begin_month_id,$end_month_id)
          0 AS is_event
        FROM $wpdb->posts
        WHERE post_status='publish'
+         AND post_type='post'
          AND post_date>='$begin_date'
          AND post_date<'$end_date'
          AND post_date<NOW()
@@ -155,7 +157,7 @@ function ec3_util_calendar_days($begin_month_id,$end_month_id)
   {
     foreach($calendar_entries as $ent)
       if($ent->is_event)
-        array_push($ignore_post_ids,$ent->id);
+        $ignore_post_ids[] = $ent->id;
   }
 
   $current_post_id=0;
