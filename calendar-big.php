f739eae02b04739c1d93496d689169a1b662592d
alex_tingle <alex_tingle@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-13 22:51:32 +0000

    Removed warning.
    git-svn-id: https://plugins.svn.wordpress.org/event-calendar/trunk@73716 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- calendar-big.php
+++ calendar-big.php
@@ -17,12 +17,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-// **************************************************
-//
-//        NON-FUNCTIONING!!   EXPERIMENTAL!!
-// 
-// **************************************************
-
 require_once(dirname(__FILE__).'/calendar-sidebar.php');
 
 /** Renders a big calendar. */
